2cb0dd131b3449e33f7c6051a8144e50da5cc664
Christian Gennari <34597557+Christian-Gennari@users.noreply.github.com>
2025-11-26 11:49:12 +0100

    .

--- Nostos.Frontend/src/app/app.routes.ts
+++ Nostos.Frontend/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { Library } from './library/library';
+import { Home } from './home/home';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
   {
     path: 'library',
     component: Library,
