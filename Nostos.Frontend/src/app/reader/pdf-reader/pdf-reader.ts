ef44139061a178dc6cfce284e49ab42e9191ba98
Christian Gennari <34597557+Christian-Gennari@users.noreply.github.com>
2025-12-01 17:14:14 +0100

    small style fixes in pdf reader

--- Nostos.Frontend/src/app/reader/pdf-reader/pdf-reader.ts
+++ Nostos.Frontend/src/app/reader/pdf-reader/pdf-reader.ts
@@ -15,6 +15,29 @@ export class PdfReader {
   // Construct the backend URL
   pdfSrc = computed(() => `/api/books/${this.bookId()}/file`);
 
+  // Define the CSS styles to be passed to the [customCSS] input
+  readonly scrollbarStyles = `
+    #toolbarViewer,
+    #toolbarViewer * {
+      font-family: "Inter", sans-serif !important;
+      letter-spacing: -0.01em;
+    }
+    #viewerContainer::-webkit-scrollbar, ::-webkit-scrollbar {
+      width: 8px;
+      height: 8px;
+    }
+    #viewerContainer::-webkit-scrollbar-track, ::-webkit-scrollbar-track {
+      background: transparent;
+    }
+    #viewerContainer::-webkit-scrollbar-thumb, ::-webkit-scrollbar-thumb {
+      background: #ddd;
+      border-radius: 4px;
+    }
+    #viewerContainer::-webkit-scrollbar-thumb:hover, ::-webkit-scrollbar-thumb:hover {
+      background: #bbb;
+    }
+  `;
+
   onPdfLoaded() {
     console.log('PDF Loaded');
   }
