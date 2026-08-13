3e923d14f61521a6464ac3debbabb67d44be398a
Christian Gennari <christiangennari61@gmail.com>
2026-08-13 11:15:27 +0200

    chore: fix stale PM2 cwd in ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'nostos',
     script: 'npm',
     args: 'run prod',
-    cwd: '/home/dev/projects/nostos-rebirth',
+    cwd: '/home/dev/coding/projects/nostos-rebirth',
     env: {
       ASPNETCORE_ENVIRONMENT: 'Production',
       ASPNETCORE_URLS: 'http://0.0.0.0:5214',
