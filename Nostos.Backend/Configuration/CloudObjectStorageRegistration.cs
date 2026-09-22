217e05e71882ee99a1e74e916febedaa56fe0374
Christian Gennari <christiangennari61@gmail.com>
2026-09-22 19:35:54 +0200

    feat(cloud): register S3-compatible asset storage

---
+++ Nostos.Backend/Configuration/CloudObjectStorageRegistration.cs
@@ -0,0 +1,45 @@
+using Amazon;
+using Amazon.Runtime;
+using Amazon.S3;
+using Nostos.Backend.Cloud.Storage;
+using Nostos.Backend.Services;
+
+namespace Nostos.Backend.Configuration;
+
+public static class CloudObjectStorageRegistration
+{
+    public static IServiceCollection AddNostosCloudObjectStorage(
+        this IServiceCollection services,
+        IConfiguration configuration,
+        Func<string, string?>? environmentReader = null)
+    {
+        var options = CloudObjectStorageOptions.FromConfiguration(configuration);
+        var credentials = options.ResolveCredentials(environmentReader);
+
+        services.AddSingleton(options);
+        services.AddSingleton<IAmazonS3>(_ =>
+        {
+            var config = new AmazonS3Config
+            {
+                ForcePathStyle = options.ForcePathStyle,
+            };
+
+            if (options.ServiceUrl is not null)
+            {
+                config.ServiceURL = options.ServiceUrl;
+                config.AuthenticationRegion = options.Region;
+            }
+            else
+            {
+                config.RegionEndpoint = RegionEndpoint.GetBySystemName(options.Region);
+            }
+
+            return new AmazonS3Client(
+                new BasicAWSCredentials(credentials.AccessKey, credentials.SecretKey),
+                config);
+        });
+
+        services.AddSingleton<IBookAssetStorage, S3BookAssetStorage>();
+        return services;
+    }
+}
